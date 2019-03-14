f551821a5bd41ed653cf0309030f6a374d348603
Grigore Lucian <lucigrigo@yahoo.com>
2019-03-14 20:40:13 +0200

    da

--- AdvancedAlgorithmsInJavaUDEMY/algorithm/src/test/java/lucian/grigore/algorithm/kmp/KnuthMorrisPrattTest.java
+++ AdvancedAlgorithmsInJavaUDEMY/algorithm/src/test/java/lucian/grigore/algorithm/kmp/KnuthMorrisPrattTest.java
@@ -1,14 +1,20 @@
 package lucian.grigore.algorithm.kmp;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class KnuthMorrisPrattTest {
 
+    private KnuthMorrisPratt knuthMorrisPratt = null;
+
+    @Before
+    public void init() {
+        knuthMorrisPratt = new KnuthMorrisPratt();
+    }
+
     @Test
     public void computeLSPTableTest() {
-        KnuthMorrisPratt knuthMorrisPratt = new KnuthMorrisPratt();
-
         String s = "ababac";
         int[] actual = knuthMorrisPratt.computeLSPTable(s.toCharArray());
         int[] expect = new int[]{0, 0, 1, 2, 3, 0};
@@ -48,5 +54,28 @@ public class KnuthMorrisPrattTest {
         actual = knuthMorrisPratt.computeLSPTable(s.toCharArray());
         expect = new int[]{0, 0};
         Assert.assertArrayEquals(expect, actual);
+    }
+
+    @Test
+    public void searchTest() {
+        String pattern = "ababac";
+        String array = "abazacabababac";
+        int index = knuthMorrisPratt.search(array.toCharArray(), pattern.toCharArray());
+        Assert.assertEquals(8, index);
+
+        pattern = "aabacaz";
+        array = "aabaaabacaz";
+        index = knuthMorrisPratt.search(array.toCharArray(), pattern.toCharArray());
+        Assert.assertEquals(4, index);
+
+        pattern = "is a";
+        array = "this is a test";
+        index = knuthMorrisPratt.search(array.toCharArray(), pattern.toCharArray());
+        Assert.assertEquals(5, index);
+
+        pattern = "foo";
+        array = "can'tfindthetest";
+        index = knuthMorrisPratt.search(array.toCharArray(), pattern.toCharArray());
+        Assert.assertEquals(-1, index);
     }
 }
