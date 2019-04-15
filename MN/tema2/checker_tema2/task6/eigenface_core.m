e64656221b816f954cb2d28766e7a2368feb2f04
Grigore Lucian <lucigrigo@yahoo.com>
2019-04-15 18:23:42 +0300

    s

--- MN/tema2/checker_tema2/task6/eigenface_core.m
+++ MN/tema2/checker_tema2/task6/eigenface_core.m
@@ -3,38 +3,23 @@ function [m A eigenfaces pr_img] = eigenface_core(database_path)
   T = [];
   for i=1:10
     temp_image = double(rgb2gray(imread(sprintf('./dataset/%d.jpg', i))));
-    vect_images = [];
-    for k=1:size(temp_image, 1)
-      line = transpose(temp_image(k, :));
-      vect_images = vertcat(vect_images, line);
-    end 
-    T = horzcat(T, vect_images);
-    % T = vertcat(T, vect_images);
+    temp_image = transpose(temp_image);
+    vect_images = temp_image(:);
+    T = [T, vect_images];
   end 
   % pasul 2
-  m = [];
-  for i=1:size(T, 1)
-    med = 0;
-    for j=1:size(T, 2)
-      med += T(i, j);
-    end 
-    med /= size(T, 2);
-    m(i) = med;
-  end
-  m = transpose(m);
-  A = T - m;  
+  m = mean(T, 2);
+  A = T - m;
   % pasul 4
-  [U, S, V] = svd((transpose(A) * A));
-  % [V D] = eig((transpose(A) * A));
+  [V, D] = eig(((A') * A));
   V_final = [];
-  for i=1:size(S, 1)
-  % for i=1:size(D, 1)
-    if S(i, i) > 1
-      V_final = horzcat(V_final, V(:, i)); 
+  diagonal_vect = diag(D);
+  for i=1:size(diagonal_vect)
+    if diagonal_vect(i) > 1
+      V_final = [V_final, V(:, i)];
     endif
-  end
-  % eigenfaces = A * V_final;
-  eigenfaces = A * V;
+  endfor
+  eigenfaces = A * V_final;
   % pasul 5
   pr_img = transpose(eigenfaces) * A;
 end
--- MN/tema2/checker_tema2/task6/face_recognition.m
+++ MN/tema2/checker_tema2/task6/face_recognition.m
@@ -1,23 +1,18 @@
 function [min_dist output_img_index] = face_recognition(image_path, m, A, eigenfaces, pr_img)
   % pasul 6
   test_img = double(rgb2gray(imread(image_path)));
-  test_img_vect = [];
-  for i=1:size(test_img, 1)
-    line = transpose(test_img(i, :));
-    test_img_vect = vertcat(test_img_vect, line);
-  end 
-  med = mean(test_img_vect);
-  test_img_vect -= med;
+  test_img = transpose(test_img);
+  test_img_vect = test_img(:);
+  test_img_vect = test_img_vect - m;
   % pasul 7
   PrTestImg = transpose(eigenfaces) * test_img_vect;
   % pasul 8
   output_img_index = -1;
   min_dist = Inf;
-  PrTestImg = transpose(PrTestImg);
-  for i=1:size(pr_img, 1)
-    if norm(PrTestImg - pr_img(i, :)) < min_dist
-      min_dist = norm(PrTestImg - pr_img(i, :));
+  for i=1:size(pr_img, 2)
+    if norm(PrTestImg - pr_img(:, i)) < min_dist
+      min_dist = norm(PrTestImg - pr_img(:, i));
       output_img_index = i;
-    endif
+    end
   end 
 end
