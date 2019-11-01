c9ac0f91896b6a2ef31e8678dc3c73cf009dd236
lucigrigo <lucigrigo@yahoo.com>
2019-11-01 23:26:25 +0200

    s

--- POO/Tema1/src/com/tema1/main/Game.java
+++ POO/Tema1/src/com/tema1/main/Game.java
@@ -1,5 +1,6 @@
 package com.tema1.main;
 
+import com.tema1.goods.Goods;
 import com.tema1.player.Player;
 
 import java.util.List;
@@ -23,26 +24,31 @@ public final class Game {
     public void startGame(List<Player> players, List<Integer> freeGoods, int maxRounds) {
         Main.utilities.setNrRounds(1);
         while (nrRounds < 5 && nrRounds < maxRounds) {
-//            System.out.println("--- RUNDA " + Main.utilities.getNrRounds() + "---");
+<<<<<<< HEAD
+            System.out.println("--- RUNDA " + Main.utilities.getNrRounds() + "---");
             // facem pe rand fiecare jucator serif
+=======
+//            System.out.println("---\tRUNDA " + Main.utilities.getNrRounds() + "\t---");
+//             facem pe rand fiecare jucator serif
+>>>>>>> df91f3181055b19c86a79eaa0fcab5c280f7f1db
             for (Player sheriffPlayer : players) {
                 for (Player nonSheriffPlayer : players) {
                     if (sheriffPlayer != nonSheriffPlayer) {
                         // fiecare jucator ia in mana 10 carti de joc
                         nonSheriffPlayer.handRefill(freeGoods);
                         // crearea sacilor celorlalti jucatori
-//                        System.out.println("CARTILE lui " + nonSheriffPlayer.getType() + " " + nonSheriffPlayer.getInitialOrderNr() + " (coins = " + nonSheriffPlayer.getCoins() + ") sunt:");
-//                        for (Goods good : nonSheriffPlayer.getOwnCards()) {
-//                            System.out.print(good.getId() + ",");
-//                        }
-//                        System.out.println();
+                        System.out.println("CARTILE lui " + nonSheriffPlayer.getType() + " " + nonSheriffPlayer.getInitialOrderNr() + " (coins = " + nonSheriffPlayer.getCoins() + ") sunt:");
+                        for (Goods good : nonSheriffPlayer.getOwnCards()) {
+                            System.out.print(good.getId() + ",");
+                        }
+                        System.out.println();
                         nonSheriffPlayer.bagCreation();
-//                        System.out.println("si baga in SAC cartile:");
-//                        for (Goods good : nonSheriffPlayer.getBag().getAssets()) {
-//                            System.out.print(good.getId() + ",");
-//                        }
-//                        System.out.println("\nbribe = " + nonSheriffPlayer.getBag().getBribe());
-//                        System.out.println("\n");
+                        System.out.println("si baga in SAC cartile:");
+                        for (Goods good : nonSheriffPlayer.getBag().getAssets()) {
+                            System.out.print(good.getId() + ",");
+                        }
+                        System.out.println("\nbribe = " + nonSheriffPlayer.getBag().getBribe());
+                        System.out.println("\n");
                     }
                 }
                 // inspectarea sacilor celorlalti jucatori
@@ -54,14 +60,14 @@ public final class Game {
     }
 
     public void endGame(List<Player> players) {
-//        System.out.println("---ITEMELE DE LA FINAL SUNT---");
-//        for (Player player : players) {
-//            System.out.println("Player " + player.getType() + " " + player.getInitialOrderNr() + " coins = " + player.getCoins());
-//            for (Goods good : player.getEndGameGoods()) {
-//                System.out.print(good.getId() + ",");
-//            }
-//            System.out.println();
-//        }
+        System.out.println("---ITEMELE DE LA FINAL SUNT---");
+        for (Player player : players) {
+            System.out.println("Player " + player.getType() + " " + player.getInitialOrderNr() + " coins = " + player.getCoins());
+            for (Goods good : player.getEndGameGoods()) {
+                System.out.print(good.getId() + ",");
+            }
+            System.out.println();
+        }
         // calcularea scorului final - fara bonusuri
         Main.utilities.computeFinalScore(players);
 
--- POO/Tema1/src/com/tema1/player/BribeStrategyPlayer.java
+++ POO/Tema1/src/com/tema1/player/BribeStrategyPlayer.java
@@ -73,64 +73,86 @@ public class BribeStrategyPlayer implements Player {
         if (coins < 10 ||
                 !Main.utilities.searchIllegalItems(ownCards)) {
             ownBag.setBribe(0);
-            if (coins >= 4) {
-//            System.out.println("aici>?");
-                List<Goods> cardsCopy = new ArrayList<>(ownCards);
-                int mostCommonAssetId = Main.utilities.findMostCommonLegalAsset(cardsCopy);
-                int potentialPenalty = 0;
-                for (int i = 0; i < cardsCopy.size(); i++) {
-                    if (cardsCopy.get(i).getId() == mostCommonAssetId) {
-                        potentialPenalty += cardsCopy.get(i).getPenalty();
-                        if (potentialPenalty > coins) {
-                            break;
-                        }
-                        ownBag.getAssets().add(cardsCopy.get(i));
-//                    cardsCopy.remove(i);
+//            if (coins < 2) {
+//            System.out.println("aici>? " + coins);
+            List<Goods> cardsCopy = new ArrayList<>(ownCards);
+            int mostCommonAssetId = Main.utilities.findMostCommonLegalAsset(cardsCopy);
+//            int potentialPenalty = 0;
+            for (int i = 0; i < cardsCopy.size(); i++) {
+                if (cardsCopy.get(i).getId() == mostCommonAssetId) {
+//                    potentialPenalty += cardsCopy.get(i).getPenalty();
+//                    if (potentialPenalty >= coins) {
+//                        break;
+//                    }
+                    ownBag.getAssets().add(cardsCopy.get(i));
+                    // TODO 2r3pm20 WTF?
+                    if (cardsCopy.get(i).getType() == GoodsType.Illegal) {
+                        break;
                     }
+//                    System.out.println(cardsCopy.get(i).getId());
+//                    cardsCopy.remove(i);
                 }
-                ownCards.removeIf((Goods g) -> g.getId() == mostCommonAssetId);
-                if (mostCommonAssetId < 20) {
-                    ownBag.setDominantAsset(mostCommonAssetId);
-                } else {
-                    ownBag.setDominantAsset(0);
-                }
-//            ownBag.setDominantAsset(mostCommonAssetId);
-//            for (int i = 0; i < ownCards.size(); i++) {
-//                if (ownCards.get(i).getId() == mostCommonAssetId) {
-//                    ownBag.getAssets().add(ownCards.get(i));
-//                    ownCards.remove(i);
-//                }
-//            }
             }
+//            ownBag.getAssets().add(ownCards.get((Goods g) -> g.getId() == mostCommonAssetId));
+//            if (mostCommonAssetId < 20) {
+//                ownBag.setDominantAsset(mostCommonAssetId);
+//            } else {
+            ownCards.removeIf((Goods g) -> g.getId() == mostCommonAssetId);
+            ownBag.setDominantAsset(mostCommonAssetId >= 20 ? 0 : mostCommonAssetId);
+//            }
+//            }
         } else {
             int nrIllegalGoods = 0;
             int possiblePenalty = 0;
             ownBag.setDominantAsset(0);
-//            ownCards.sort((Goods good1, Goods good2) ->
-//                    good2.getProfit() - good1.getProfit());
-//            ownCards.sort(Comparator.comparing(Goods::getProfit).thenComparing(Goods::getId));
-//            Collections.reverse(ownCards);
             List<Goods> ownCardsCopy = new ArrayList<>(ownCards);
 //            ownCardsCopy.sort((Goods g1, Goods g2) -> g2.getProfit() - g1.getProfit());
             ownCardsCopy.sort(Comparator.comparing(Goods::getProfit).thenComparing(Goods::getId));
             Collections.reverse(ownCardsCopy);
+//            possiblePenalty += ownCardsCopy.get(0).getPenalty();
             while (!ownCardsCopy.isEmpty()
                     && ownCardsCopy.get(0).getType() == GoodsType.Illegal
-                    && ownBag.getAssets().size() < 8
-                    && possiblePenalty < coins - 4) {
-                ownBag.getAssets().add(ownCardsCopy.get(0));
-//                System.out.println("se adauga" + ownCardsCopy.get(0).getId());
+                    && ownBag.getAssets().size() < 8) {
                 possiblePenalty += ownCardsCopy.get(0).getPenalty();
+                if (possiblePenalty >= coins) {
+                    possiblePenalty -= ownCardsCopy.get(0).getPenalty();
+                    break;
+                }
+//                System.out.println("se adauga" + ownCardsCopy.get(0).getId());
+                ownBag.getAssets().add(ownCardsCopy.get(0));
                 ownCardsCopy.remove(0);
                 nrIllegalGoods++;
             }
+//            if (possiblePenalty >= coins) {
+//            }
+//            ownBag.setDominantAsset(0);
+//            while (!ownCardsCopy.isEmpty() &&
+//                    ownBag.getAssets().size() < 8) {
+//                    && possiblePenalty < coins - 2) {
+//                System.out.println(possiblePenalty);
+            for (int i = 0; i < ownCardsCopy.size() && ownBag.getAssets().size() < 8; i++) {
+                if (ownCardsCopy.get(i).getType() == GoodsType.Legal) {
+                    possiblePenalty += ownCardsCopy.get(i).getPenalty();
+//                System.out.println(possiblePenalty);
+                    if (possiblePenalty >= coins) {
+                        break;
+                    }
+                    ownBag.getAssets().add(ownCardsCopy.get(i));
+//                System.out.println("se adauga" + ownCardsCopy.get(0).getId());
+//                    ownCardsCopy.remove(i);
+                }
+            }
+
             if (nrIllegalGoods <= 2) {
                 ownBag.setBribe(5);
                 coins -= 5;
+//                    System.out.println("si aicea");
             } else {
                 ownBag.setBribe(10);
                 coins -= 10;
+<<<<<<< HEAD
             }
+//            possiblePenalty -=
             ownBag.setDominantAsset(0);
             while (!ownCardsCopy.isEmpty() &&
                     ownBag.getAssets().size() < 8
@@ -138,14 +160,11 @@ public class BribeStrategyPlayer implements Player {
                 possiblePenalty += ownCardsCopy.get(0).getPenalty();
                 ownBag.getAssets().add(ownCardsCopy.get(0));
                 ownCardsCopy.remove(0);
+=======
+//                    System.out.println("aici de cate ori?");
+>>>>>>> df91f3181055b19c86a79eaa0fcab5c280f7f1db
             }
         }
-//        System.out.println("itemele lui " + getType() + getInitialOrderNr());
-        for (Goods good : ownBag.getAssets()) {
-//            System.out.print(good.getId() + ",");
-        }
-//        System.out.print(ownBag.getBribe());
-//        System.out.println("\n");
     }
 
     @Override
@@ -164,11 +183,9 @@ public class BribeStrategyPlayer implements Player {
                             if (Main.utilities.searchIllegalItems(player.getBag().getAssets(), player.getBag())) {
                                 Main.utilities.confiscateBag(this, player, freeGoods);
                             } else {
-//                            System.out.println("mereu aici 4 ori");
                                 Main.utilities.payPenalty(this, player);
                             }
                         }
-
                     } else {
 //                        System.out.println("jucatorul " + this.getType() + " " + this.getInitialOrderNr()
 //                                + " nu mai are bani de inspectat pe " + player.getType() + " " + player.getInitialOrderNr());
@@ -179,6 +196,8 @@ public class BribeStrategyPlayer implements Player {
                 } else {
 //                    System.out.println("se si accepta mita?");
                     Main.utilities.acceptBribe(this, player);
+//                    Main.utilities.acceptBribe(this, player);
+
                 }
             }
         }
