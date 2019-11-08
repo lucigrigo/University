5d5effabe58462ee82c6d3ba800cb0af0e9f6cb0
lucigrigo <lucigrigo@yahoo.com>
2019-11-08 09:13:54 +0200

    oldC3proj-wip

---
+++ C#/Proiecte C#/Fractali/Triangle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Fractali
+{
+    class Triangle
+    {
+        private PointF X, Y, Z;
+        private Color K=Color.White;
+        public Triangle(PointF A, PointF B, PointF C)
+        {
+            X = A;
+            Y = B;
+            Z = C;
+        }
+        public Triangle(PointF A, PointF B, PointF C,Color c)
+        {
+            X = A;
+            Y = B;
+            Z = C;
+            K = c;
+        }
+        public void Draw(Graphics g)
+        {
+            Pen p = new Pen(K, 2);
+            g.DrawLine(p, X, Y);
+            g.DrawLine(p, X, Z);
+            g.DrawLine(p, Y, Z);
+        }
+        public PointF GetX()
+        {
+            return X;
+        }
+        public PointF GetY()
+        {
+            return Y;
+        }
+        public PointF GetZ()
+        {
+            return Z;
+        }
+
+    }
+}
---
+++ C#/Proiecte C#/LeeSoarece/LeeSoarece/ReprezentareaInformatiilor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LeeSoarece
+{
+    public partial class ReprezentareaInformatiilor : Form
+    {
+        int n, m;
+        int[,] A = new int[100, 100];
+        public ReprezentareaInformatiilor()
+        {
+            InitializeComponent();
+
+        }
+
+        private void ReprezentareaInformatiilor_Load(object sender, EventArgs e)
+        {
+            Incarcare();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            n = Convert.ToInt32(textBox1.Text);
+            m = Convert.ToInt32(textBox2.Text);
+            for(int k=0;k<n;k++)
+            {
+                for(int j=0;j<m;j++)
+                {
+                    A[k,j]=richTextBox1.Text.
+                }
+            }
+        }
+
+        private void Incarcare()
+        {
+
+        }
+    }
+}
