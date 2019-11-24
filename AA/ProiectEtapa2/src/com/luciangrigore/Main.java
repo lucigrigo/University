8b4b9f2539ac8930398cc751f93c1f0c40f7706a
lucigrigo <lucigrigo@yahoo.com>
2019-11-24 02:01:40 +0200

    strAa

--- AA/ProiectEtapa2/src/com/luciangrigore/Main.java
+++ AA/ProiectEtapa2/src/com/luciangrigore/Main.java
@@ -3,10 +3,31 @@ package com.luciangrigore;
 public class Main {
 
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
 
 //        String test = "abcdef";
 //        new GenerateSubsequences().generateSubsequences(test, 0, new int[test.length()]);
 
+        if (args[0] != null) {
+            switch (args[0]) {
+                case "tests":
+                    // run all tests
+                    // input dir, output dir
+                    break;
+                case "manual":
+                    // apply both algorithms to search given pattern
+                    // into given string
+                    break;
+                case "subseq":
+                    // TODO decide whether or not this is to be done
+                    break;
+                default:
+                    System.out.println("Input invalid!");
+                    System.exit(1);
+            }
+        } else {
+            System.out.println("Input invalid!");
+            System.exit(1);
+        }
     }
 }
