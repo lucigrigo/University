7c8918c2807da6143ca1fe4d63fc4a5bcb71ba5b
Grigore Lucian <lucigrigo@yahoo.com>
2020-01-14 18:52:22 +0200

    Python sample using Tensorflow

--- TestSources/python_sample.py
+++ TestSources/python_sample.py
@@ -1,22 +1,36 @@
-# Python sample code intended towards practice
+# Python sample code source intended towards practice
 
 # Just disables the warning, doesn't enable AVX/FMA
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-print("\n --- math module")
-from tensorflow import math
-a = math.imag(-2.54 + 6.82j)
+# imports from TENSORFLOW API
+print("---------------------------------------------------------------")
+print("                --- Using TENSORFLOW framework ---\n")
+import tensorflow as tf
+# from tensorflow import Module
+# from tensorflow import math
+# from tensorflow import signal
+# from tensorflow_core import Tensor
+
+# Math module
+print("\n === Math module")
+a = tf.math.imag(-2.54 + 6.82j)
 print(a)
-a = math.imag(10)
+a = tf.math.imag(10)
 print(a)
 
-print("\n --- 'Module' module - neural networks related")
-from tensorflow import Module as tensorflowModule
-module = tensorflowModule()
-# TODO add methods?
+# Neural network module
+print("\n === 'Module' module - neural networks related")
+module = tf.Module()
+print(module)
+# TODO get used to 'Module()' methods and fields and add them here
 
-print("\n --- signal module")
-from tensorflow import signal
-b = signal.fft3d([1.3, 5.6, -2.6])
-print(b)
+# signal analysis module
+print("\n === Signal module")
+# TODO create Operation, Value Index and DType for our Tensor to work
+# c = tf.Tensor()
+# b = signal.fft3d(c)
+# print(b)
+
+print("\n---------------------------------------------------------------")
