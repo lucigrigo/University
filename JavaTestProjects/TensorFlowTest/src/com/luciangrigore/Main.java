f71c7617dead31e3c08ed6e6070bd32bf68ab0fe
lucigrigo <lucigrigo@yahoo.com>
2020-01-17 00:48:28 +0200

    updates on folders and added new java project for tensorflow study

---
+++ JavaTestProjects/TensorFlowTest/src/com/luciangrigore/Main.java
@@ -0,0 +1,15 @@
+/*
+                             Testing TENSORFLOW Framework
+ */
+package com.luciangrigore;
+
+// TODO add tensorflow dependency
+//import org.tensorflow;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+}
