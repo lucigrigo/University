3cc9a26b4583a6990da8519c4253e334601e168f
Grigore Lucian <lucigrigo@yahoo.com>
2020-01-26 14:13:02 +0200

    nlp using bag of words model used on restaurant reviews

--- MachineLearningA-ZUDEMY/Machine-Learning-A-Z-New/Machine Learning A-Z New/Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/nlp.py
+++ MachineLearningA-ZUDEMY/Machine-Learning-A-Z-New/Machine Learning A-Z New/Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/nlp.py
@@ -9,3 +9,43 @@ import matplotlib.pyplot as plt
 dataset = pd.read_csv("Restaurant_Reviews.tsv", delimiter = '\t', quoting = 3)
 
 # Cleaning the texts
+import re # regular expressions manipulation library
+import nltk # natural language toolkit library
+nltk.download('stopwords') # downloading list of irrelevant words
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+corpus = []
+for i in range(0, len(dataset)):
+    # only keeping the letters in the review
+    review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
+    # putting all letters in lower case
+    review = review.lower()
+    # removing useless words & stemming the remaining words
+    review = review.split()
+    ps = PorterStemmer()
+    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+    review = ' '.join(review)
+    corpus.append(review)
+
+# Creating the Bag of Words model
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features = 1500)
+X = cv.fit_transform(corpus).toarray()
+y = dataset.iloc[:, 1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+# Fitting the classifier to the Training Set
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+
+# Predicting the test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+(55 + 91)/200 
