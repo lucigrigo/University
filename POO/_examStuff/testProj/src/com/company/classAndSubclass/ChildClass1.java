58fec32c6c294691594340d0f6af420a4ed552fb
Grigore Lucian <lucigrigo@yahoo.com>
2020-02-02 18:52:29 +0200

    java test project for poo exam

---
+++ POO/_examStuff/testProj/src/com/company/classAndSubclass/ChildClass1.java
@@ -0,0 +1,18 @@
+package com.company.classAndSubclass;
+
+public class ChildClass1 extends ParentClass {
+
+    public void tryReachOutForPapa() {
+        // can use protected methods & members
+        super.nrProtected = 100;
+        super.doNothingProtected();
+
+        // and public ones
+        super.nrPublic = 100;
+        super.publicMethod();
+
+        // and default ones
+        super.nrPackageProtected = 100;
+        super.packageProtectedMethod();
+    }
+}
