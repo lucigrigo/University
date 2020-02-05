de69e1052b47c0d83bdb9f5eb22fd9a605b2e7be
Grigore Lucian <luci.grigore99@gmail.com>
2020-02-05 15:40:10 +0200

    ex1 v1 exam iocla

--- IOCLA/_examStuff/ModelV1/1/cross_add.asm
+++ IOCLA/_examStuff/ModelV1/1/cross_add.asm
@@ -2,10 +2,14 @@
 
 section .data
     ; TODO a: Define arr1, arr2 and res arrays.
-
+    arr1 dd 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+    arr2 dd 30, 35, 40, 45, 50, 55, 60, 55, 60, 65
 
 section .bss
     res resd 10
+    
+section .rodata
+    num dd 10
 
 section .text
 global main
@@ -16,8 +20,16 @@ main:
 
 
     ; TODO b: Compute res[0] and res[n-1].
-
-
+    ; res[0]
+    xor eax, eax
+    mov eax, [arr1]
+    add eax, [arr2 + 36]
+    mov [res], eax
+    ; res[n-1]
+    xor eax, eax
+    mov eax, [arr1 + 36]
+    add eax, [arr2]
+    mov [res + 36], eax
 
     ; List first and last item in each array.
     PRINT_UDEC 4, [arr1]
@@ -36,12 +48,34 @@ main:
 
 
     ; TODO d: Compute cross sums in res[i].
-
-
+    xor ecx, ecx
+compute_cross_sum:
+    xor eax, eax
+    add eax, [arr1 + 4 * ecx]
+    mov ebx, ecx
+    neg ebx
+    add eax, [arr2 + 36 + 4 * ebx]
+    mov [res + 4 * ecx], eax
+    
+    inc ecx
+    cmp ecx, [num]
+    jl compute_cross_sum
 
     ; TODO c: List arrays.
-
-
+    xor ecx, ecx
+print_arrays:
+    PRINT_UDEC 4, [arr1 + 4 * ecx]
+    PRINT_STRING " + "
+    mov ebx, ecx
+    neg ebx
+    PRINT_UDEC 4, [arr2 + 36 + 4 * ebx]
+    PRINT_STRING " = "
+    PRINT_UDEC 4, [res + 4 * ecx]
+    NEWLINE
+    
+    inc ecx
+    cmp ecx, [num]
+    jl print_arrays
 
     ; Return 0.    
     xor eax, eax
