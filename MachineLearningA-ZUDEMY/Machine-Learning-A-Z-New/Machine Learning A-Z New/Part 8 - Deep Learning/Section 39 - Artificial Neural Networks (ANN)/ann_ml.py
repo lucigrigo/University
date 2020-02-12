961f9bcfa3be343e5a8499e102d4ef6b331f13c0
Grigore Lucian <lucigrigo@yahoo.com>
2020-02-12 22:15:05 +0200

    ann ml lesson

--- MachineLearningA-ZUDEMY/Machine-Learning-A-Z-New/Machine Learning A-Z New/Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/ann_ml.py
+++ MachineLearningA-ZUDEMY/Machine-Learning-A-Z-New/Machine Learning A-Z New/Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/ann_ml.py
@@ -4,14 +4,14 @@
 
 # Part 1 - Data Preprocessing
 
-# Importing the libraries
+# Importing  libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Churn_Modelling.csv')
-X = dataset.iloc[:, 3 : 13].values
+X = dataset.iloc[:, 3 : 13].values 
 y = dataset.iloc[:, 13].values
 
 # Encoding categorical data
@@ -19,11 +19,14 @@ from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X_1 = LabelEncoder()
 X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1]) 
 labelencoder_X_2 = LabelEncoder()
-X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1]) 
+X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2]) 
+onehotencoder = OneHotEncoder(categorical_features = [1])
+X = onehotencoder.fit_transform(X).toarray()
+X = X[:, 1:] # dummy variable trap removal
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
@@ -31,11 +34,36 @@ sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 
-# Fitting the classifier to the Training Set
-# Create your classifier here
+# Part 2 - Creating the ANN
+
+# Importing libraries
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+# ANN initialisation
+classifier = Sequential()
+
+# Adding the input layer and the first hidden layer
+classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_shape = (11,)))
+
+# Adding the second hidden layer
+classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
+
+# Adding the output layer
+classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+
+# Compiling the ANN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# Fitting the ANN to the Training set
+classifier.fit(X_train, y_train, batch_size = 10, epochs = 200)
+
+# Part 3 - Making the predictions and evaluating the model
 
 # Predicting the test set results
 y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
 
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
