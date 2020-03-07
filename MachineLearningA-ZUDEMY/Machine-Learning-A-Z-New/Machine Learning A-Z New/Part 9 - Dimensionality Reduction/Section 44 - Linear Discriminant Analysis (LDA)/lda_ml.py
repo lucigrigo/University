31feaa2128e96619ae63d2d6ad8a258ce70065e2
lucigrigo <lucigrigo@yahoo.com>
2020-03-07 11:09:49 +0200

    lda in python fixed misstyping

--- MachineLearningA-ZUDEMY/Machine-Learning-A-Z-New/Machine Learning A-Z New/Part 9 - Dimensionality Reduction/Section 44 - Linear Discriminant Analysis (LDA)/lda_ml.py
+++ MachineLearningA-ZUDEMY/Machine-Learning-A-Z-New/Machine Learning A-Z New/Part 9 - Dimensionality Reduction/Section 44 - Linear Discriminant Analysis (LDA)/lda_ml.py
@@ -51,8 +51,8 @@ for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green', 'blue'))(i), label = j)
 plt.title('Logistic Regression (Training set)')
-plt.xlabel('PC1')
-plt.ylabel('PC2')
+plt.xlabel('LD1')
+plt.ylabel('LD2')
 plt.legend()
 plt.show()
 
@@ -69,7 +69,7 @@ for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green', 'blue'))(i), label = j)
 plt.title('Logistic Regression (Test set)')
-plt.xlabel('PC1')
-plt.ylabel('PC2')
+plt.xlabel('LD1')
+plt.ylabel('LD2')
 plt.legend()
 plt.show()
