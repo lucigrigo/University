f5c56f69b85bf9d269a821ecabb72b24bf683d36
Grigore Lucian <lucigrigo@yahoo.com>
2020-03-11 19:31:52 +0200

    lab4 pco

--- PCo/Lab04/router.c
+++ PCo/Lab04/router.c
@@ -13,7 +13,20 @@ int arp_table_len;
 */
 struct route_table_entry *get_best_route(__u32 dest_ip) {
 	/* TODO 1: Implement the function */
-	return NULL;
+	int i = 0;
+	uint32_t mask_max = -1;
+	int index = -1;
+
+	while(i < rtable_size) {
+		uint32_t temp = rtable[i].mask & dest_ip;
+		if(temp == rtable[i].prefix && rtable[i].mask > mask_max) {
+			mask_max = rtable[i].mask;
+			index = i;
+		}
+		i++;
+	}
+
+	return (index == -1) ? NULL : &rtable[index];
 }
 
 /*
@@ -21,8 +34,18 @@ struct route_table_entry *get_best_route(__u32 dest_ip) {
  for the given dest_ip or NULL if there is no matching entry.
 */
 struct arp_entry *get_arp_entry(__u32 ip) {
-    /* TODO 2: Implement */
-    return NULL;
+    	/* TODO 2: Implement */
+	int i = 0;
+	int index = -1;
+
+	while(i < arp_table_len) {
+		if(ip == arp_table[i].ip) {
+			index = i;
+		}
+		i++;
+	}
+
+    	return (index == -1) ? NULL : &arp_table[index];
 }
 
 int main(int argc, char *argv[])
@@ -44,15 +67,39 @@ int main(int argc, char *argv[])
 		struct ether_header *eth_hdr = (struct ether_header *)m.payload;
 		struct iphdr *ip_hdr = (struct iphdr *)(m.payload + sizeof(struct ether_header));
 		/* TODO 3: Check the checksum */
+		if(ip_checksum(ip_hdr, sizeof(struct iphdr))) {
+			printf("checksum error!\n");
+			continue;
+		}
 
 		/* TODO 4: Check TTL >= 1 */
+		if(!ip_hdr->ttl) {
+			printf("ttl error!\n");
+			continue;
+		}
 
 		/* TODO 5: Find best matching route (using the function you wrote at TODO 1) */
+		uint32_t destination_ip = ip_hdr->daddr;
+		struct route_table_entry *best_route = get_best_route(destination_ip);
+		if(best_route == NULL) {
+			printf("rtable_route wrong!\n");
+			continue;	
+		}
 
 		/* TODO 6: Update TTL and recalculate the checksum */
+		ip_hdr->ttl = ip_hdr->ttl - 1;
+		ip_hdr->check = 0;
+		ip_hdr->check = ip_checksum(ip_hdr, sizeof(struct iphdr));
 
 		/* TODO 7: Find matching ARP entry and update Ethernet addresses */
+		struct arp_entry *best_arp_route = get_arp_entry(destination_ip);
+		if(best_arp_route == NULL) {
+			printf("arp route wrong!\n");
+			continue;
+		}
+		memcpy(eth_hdr->ether_dhost, best_arp_route->mac, sizeof*eth_hdr->ether_dhost);
 
 		/* TODO 8: Forward the pachet to best_route->interface */
+		send_packet(best_route->interface, &m);
 	}
 }
