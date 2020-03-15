dcb1b26b92fcc236a71028595cdae13604f85cbb
Grigore Lucian <lucigrigo@yahoo.com>
2020-03-15 10:54:37 +0200

    reformated labs 2,3 & 4 PA

--- PA/Lab3/lab3.c
+++ PA/Lab3/lab3.c
@@ -1,197 +1,170 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void problema_rucsacului(float m[], float v[], int len, int max)
-{
-    float p[len];
-    int k;
-    // initializare ponderi
-    for (k = 0; k < len; k++)
-    {
-        p[k] = v[k] / m[k];
-    }
+void problema_rucsacului(float m[], float v[], int len, int max) {
+  float p[len];
+  int k;
+  // initializare ponderi
+  for (k = 0; k < len; k++) {
+    p[k] = v[k] / m[k];
+  }
 
-    // sortare descrescatoare dupa ponderi
-    for (k = 0; k < (len - 1); k++)
-    {
-        int j = k + 1;
-        for (; j < len; j++)
-        {
-            if (p[j] > p[k])
-            {
-                int temp = p[k];
-                p[k] = p[j];
-                p[j] = temp;
-                temp = v[k];
-                v[k] = v[j];
-                v[j] = temp;
-                temp = m[k];
-                m[k] = m[j];
-                m[j] = temp;
-            }
+  // sortare descrescatoare dupa ponderi
+  for (k = 0; k < (len - 1); k++) {
+    int j = k + 1;
+    for (; j < len; j++) {
+      if (p[j] > p[k]) {
+        int temp = p[k];
+        p[k] = p[j];
+        p[j] = temp;
+        temp = v[k];
+        v[k] = v[j];
+        v[j] = temp;
+        temp = m[k];
+        m[k] = m[j];
+        m[j] = temp;
+      }
+    }
+  }
+
+  // umplere rucsac
+  float cant = 0, val = 0;
+  while (cant < max) {
+    for (k = 0; k < len; k++) {
+      if (cant + m[k] <= max) {
+        cant += m[k];
+        val += v[k];
+        printf("Se adauga in totalitate elem cu masa %.2f\n", m[k]);
+        if (cant == max) {
+          break;
         }
-    }
-
-    // umplere rucsac
-    float cant = 0, val = 0;
-    while (cant < max)
-    {
-        for (k = 0; k < len; k++)
-        {
-            if (cant + m[k] <= max)
-            {
-                cant += m[k];
-                val += v[k];
-                printf("Se adauga in totalitate elem cu masa %.2f\n", m[k]);
-                if (cant == max)
-                {
-                    break;
-                }
-            }
-            if (cant + m[k] > max)
-            {
-                float procent = (max - cant) / m[k];
-                cant += m[k] * procent;
-                val += v[k] * procent;
-                printf("Se adauga in proportie de %.3f elem cu masa %.2f\n", procent, m[k]);
-                break;
-            }
-        }
-    }
-    printf("Valoare totala rucsac = %.2f\n", val);
-}
-
-void problema_spectacolelor(float start[], float final[], int nr_filme)
-{
-    int k;
-    // ordonare crescatoare dupa timpul de terminare al filmelor
-    for (k = 0; k < (nr_filme - 1); k++)
-    {
-        int j = k + 1;
-        for (; j < nr_filme; j++)
-        {
-            if (final[j] < final[k])
-            {
-                int temp = final[j];
-                final[j] = final[k];
-                final[k] = temp;
-                temp = start[j];
-                start[j] = start[k];
-                start[k] = temp;
-            }
-        }
-    }
-
-    // punere in redare filme
-    int final_spectacol = 0, nr = 0;
-    for (k = 0; k < nr_filme; k++)
-    {
-        if (start[k] >= final_spectacol)
-        {
-            nr++;
-            final_spectacol = final[k];
-            printf("Se reda filmul de la %.2f la %.2f\n", start[k], final[k]);
-        }
-    }
-    printf("S-au redat in total %d filme\n", nr);
-}
-
-void problema_cuburi(int cul[], int len[], int nr)
-{
-    int k;
-    // sortare crescator dupa lungimi
-    for (k = 0; k < (nr - 1); k++)
-    {
-        int j = k + 1;
-        for (; j < nr; j++)
-        {
-            if (len[j] > len[k])
-            {
-                int temp = len[j];
-                len[j] = len[k];
-                len[k] = temp;
-                temp = cul[j];
-                cul[j] = cul[k];
-                cul[k] = temp;
-            }
-        }
-    }
-
-    // construire turn
-    int inaltime = 0, ultima_culoare = -1, ultima_lungime = 10000000;
-
-    for (k = 0; k < nr; k++)
-    {
-        if (ultima_culoare != cul[k] && ultima_lungime >= len[k])
-        {
-            ultima_culoare = cul[k];
-            ultima_lungime = len[k];
-            inaltime += len[k];
-            printf("Se asaza cubul cu latura %d si culoare %d\n", len[k], cul[k]);
-        }
-    }
-
-    printf("Inaltimea turnului construit este %d\n", inaltime);
-}
-
-void problema_segmente(int start[], int final[], int culori[], int nr)
-{
-    int k;
-    // ordonare crescator dupa finalul segmentului
-    for (k = 0; k < (nr - 1); k++)
-    {
-        int j = k + 1;
-        for (; j < nr; j++)
-        {
-            if (final[j] < final[k])
-            {
-                int temp = final[j];
-                final[j] = final[k];
-                final[k] = temp;
-                temp = start[j];
-                start[j] = start[k];
-                start[k] = temp;
-            }
-        }
-    }
-
-    int c[nr], seg_valide[nr];
-    while (1)
-    {
+      }
+      if (cant + m[k] > max) {
+        float procent = (max - cant) / m[k];
+        cant += m[k] * procent;
+        val += v[k] * procent;
+        printf("Se adauga in proportie de %.3f elem cu masa %.2f\n", procent,
+               m[k]);
         break;
+      }
     }
+  }
+  printf("Valoare totala rucsac = %.2f\n", val);
 }
 
-int main()
-{
-    // PROBLEMA RUCSACULUI
-    float m[] = {10, 30, 20};
-    float v[] = {60, 120, 100};
-    int nr_obj = 3;
-    int max = 50;
-    printf("\n--- PROBLEMA RUCSACULUI ---\n");
-    problema_rucsacului(m, v, nr_obj, max);
+void problema_spectacolelor(float start[], float final[], int nr_filme) {
+  int k;
+  // ordonare crescatoare dupa timpul de terminare al filmelor
+  for (k = 0; k < (nr_filme - 1); k++) {
+    int j = k + 1;
+    for (; j < nr_filme; j++) {
+      if (final[j] < final[k]) {
+        int temp = final[j];
+        final[j] = final[k];
+        final[k] = temp;
+        temp = start[j];
+        start[j] = start[k];
+        start[k] = temp;
+      }
+    }
+  }
 
-    // PROBLEMA SPECTACOLELOR
-    float start[] = {10, 11, 12, 14, 16, 17};
-    float final[] = {11, 13, 13, 18, 17, 19};
-    int nr_filme = 6;
-    printf("\n--- PROBLEMA SPECTACOLELOR ---\n");
-    problema_spectacolelor(start, final, nr_filme);
+  // punere in redare filme
+  int final_spectacol = 0, nr = 0;
+  for (k = 0; k < nr_filme; k++) {
+    if (start[k] >= final_spectacol) {
+      nr++;
+      final_spectacol = final[k];
+      printf("Se reda filmul de la %.2f la %.2f\n", start[k], final[k]);
+    }
+  }
+  printf("S-au redat in total %d filme\n", nr);
+}
 
-    // PROBLEMA CUBURI
-    int culori[] = {0, 1, 2, 0, 1, 2, 0, 1};
-    int lungimi[] = {3, 2, 3, 1, 6, 4, 3, 1};
-    int nr_cuburi = 8;
-    printf("\n--- PROBLEMA CUBURILOR ---\n");
-    problema_cuburi(culori, lungimi, nr_cuburi);
+void problema_cuburi(int cul[], int len[], int nr) {
+  int k;
+  // sortare crescator dupa lungimi
+  for (k = 0; k < (nr - 1); k++) {
+    int j = k + 1;
+    for (; j < nr; j++) {
+      if (len[j] > len[k]) {
+        int temp = len[j];
+        len[j] = len[k];
+        len[k] = temp;
+        temp = cul[j];
+        cul[j] = cul[k];
+        cul[k] = temp;
+      }
+    }
+  }
 
-    // PROBLEMA SEGMENTELOR
-    int begin[] = {};
-    int end[] = {};
-    int culori_segmente[] = {};
-    int nr_segmente = 0;
-    printf("\n--- PROBLEMA SEGMENTELOR ---\n");
-    problema_segmente(begin, end, culori_segmente, nr_segmente);
-    return 0;
+  // construire turn
+  int inaltime = 0, ultima_culoare = -1, ultima_lungime = 10000000;
+
+  for (k = 0; k < nr; k++) {
+    if (ultima_culoare != cul[k] && ultima_lungime >= len[k]) {
+      ultima_culoare = cul[k];
+      ultima_lungime = len[k];
+      inaltime += len[k];
+      printf("Se asaza cubul cu latura %d si culoare %d\n", len[k], cul[k]);
+    }
+  }
+
+  printf("Inaltimea turnului construit este %d\n", inaltime);
+}
+
+void problema_segmente(int start[], int final[], int culori[], int nr) {
+  int k;
+  // ordonare crescator dupa finalul segmentului
+  for (k = 0; k < (nr - 1); k++) {
+    int j = k + 1;
+    for (; j < nr; j++) {
+      if (final[j] < final[k]) {
+        int temp = final[j];
+        final[j] = final[k];
+        final[k] = temp;
+        temp = start[j];
+        start[j] = start[k];
+        start[k] = temp;
+      }
+    }
+  }
+
+  int c[nr], seg_valide[nr];
+  while (1) {
+    break;
+  }
+}
+
+int main() {
+  // PROBLEMA RUCSACULUI
+  float m[] = {10, 30, 20};
+  float v[] = {60, 120, 100};
+  int nr_obj = 3;
+  int max = 50;
+  printf("\n--- PROBLEMA RUCSACULUI ---\n");
+  problema_rucsacului(m, v, nr_obj, max);
+
+  // PROBLEMA SPECTACOLELOR
+  float start[] = {10, 11, 12, 14, 16, 17};
+  float final[] = {11, 13, 13, 18, 17, 19};
+  int nr_filme = 6;
+  printf("\n--- PROBLEMA SPECTACOLELOR ---\n");
+  problema_spectacolelor(start, final, nr_filme);
+
+  // PROBLEMA CUBURI
+  int culori[] = {0, 1, 2, 0, 1, 2, 0, 1};
+  int lungimi[] = {3, 2, 3, 1, 6, 4, 3, 1};
+  int nr_cuburi = 8;
+  printf("\n--- PROBLEMA CUBURILOR ---\n");
+  problema_cuburi(culori, lungimi, nr_cuburi);
+
+  // PROBLEMA SEGMENTELOR
+  int begin[] = {};
+  int end[] = {};
+  int culori_segmente[] = {};
+  int nr_segmente = 0;
+  printf("\n--- PROBLEMA SEGMENTELOR ---\n");
+  problema_segmente(begin, end, culori_segmente, nr_segmente);
+  return 0;
 }
--- PA/Lab4/lab4.c
+++ PA/Lab4/lab4.c
@@ -1,105 +1,88 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #define max(a, b) (((a) > (b)) ? (a) : (b))
 
-int D(int capacitate_curenta, int index, int m[], int v[], int obj[])
-{
-    if (index < 1)
-    {
-        return 0;
-    }
-    if (capacitate_curenta < 0)
-    {
-        return 0;
-    }
-    if (D(capacitate_curenta, index - 1, m, v, obj) <
-        D(capacitate_curenta - m[index], index - 1, m, v, obj) + v[index])
-    {
-        obj[index] = m[index];
-    }
-
-    return max(D(capacitate_curenta, index - 1, m, v, obj),
-               D(capacitate_curenta - m[index], index - 1, m, v, obj) + v[index]);
-}
-
-void problema_rucsacului_discret(int m[], int v[], int n, int max)
-{
-
-    int *obj = (int *)malloc(n * sizeof(int));
-    printf("valoare maxima este: %d\n", D(max, n - 1, m, v, obj));
-}
-
-void problema_subsir(int v[], int n)
-{
-    int D[n];
-    int i;
-
-    for (i = 0; i < n; i++)
-    {
-        D[i] = 0;
-    }
-
-    for (i = 0; i < n; i++)
-    {
-        int j = 0;
-        int max = 0;
-        for (; j < i; j++)
-        {
-            if (D[j] > max && v[i] > v[j])
-            {
-                max = D[j];
-            }
-        }
-        D[i] = max + 1;
-    }
-
-    int max = -1;
-    for (i = 0; i < n; i++)
-    {
-        if (D[i] > max)
-        {
-            max = D[i];
-        }
-    }
-    printf("lungimea maxima este %d\n", max);
-    printf("si vectorul cu lungimi este:\n\t");
-    for (i = 0; i < n; i++)
-    {
-        printf("%d ", v[i]);
-    }
-    printf("\n\t");
-    for (i = 0; i < n; i++)
-    {
-        printf("%d ", D[i]);
-    }
-    printf("\n");
-    int curent_max = 1000000;
-    printf("subsirul solutie este:\n\t");
-    for (i = n - 1; i >= 0; i--)
-    {
-        if (D[i] == max && curent_max > v[i])
-        {
-            printf("%d ", v[i]);
-            max--;
-            curent_max = v[i];
-        }
-    }
-    printf("\n");
-}
-
-int main()
-{
-    printf("\n---------------------------------------------\n");
-
-    printf("\n--- PROBLEMA RUCSACULUI DISCRET ---\n");
-    int m[] = {10, 30, 20};
-    int v[] = {60, 120, 100};
-    problema_rucsacului_discret(m, v, 3, 50);
-
-    printf("\n--- PROBLEMA CELUI MAI LUNG SUBSIR ---\n");
-    int v1[] = {6, 1, 2, 9, 3, 8, 4, 7};
-    problema_subsir(v1, 8);
-
-    printf("\n---------------------------------------------\n");
+int D(int capacitate_curenta, int index, int m[], int v[], int obj[]) {
+  if (index < 1) {
     return 0;
+  }
+  if (capacitate_curenta < 0) {
+    return 0;
+  }
+  if (D(capacitate_curenta, index - 1, m, v, obj) <
+      D(capacitate_curenta - m[index], index - 1, m, v, obj) + v[index]) {
+    obj[index] = m[index];
+  }
+
+  return max(D(capacitate_curenta, index - 1, m, v, obj),
+             D(capacitate_curenta - m[index], index - 1, m, v, obj) + v[index]);
+}
+
+void problema_rucsacului_discret(int m[], int v[], int n, int max) {
+
+  int *obj = (int *)malloc(n * sizeof(int));
+  printf("valoare maxima este: %d\n", D(max, n - 1, m, v, obj));
+}
+
+void problema_subsir(int v[], int n) {
+  int D[n];
+  int i;
+
+  for (i = 0; i < n; i++) {
+    D[i] = 0;
+  }
+
+  for (i = 0; i < n; i++) {
+    int j = 0;
+    int max = 0;
+    for (; j < i; j++) {
+      if (D[j] > max && v[i] > v[j]) {
+        max = D[j];
+      }
+    }
+    D[i] = max + 1;
+  }
+
+  int max = -1;
+  for (i = 0; i < n; i++) {
+    if (D[i] > max) {
+      max = D[i];
+    }
+  }
+  printf("lungimea maxima este %d\n", max);
+  printf("si vectorul cu lungimi este:\n\t");
+  for (i = 0; i < n; i++) {
+    printf("%d ", v[i]);
+  }
+  printf("\n\t");
+  for (i = 0; i < n; i++) {
+    printf("%d ", D[i]);
+  }
+  printf("\n");
+  int curent_max = 1000000;
+  printf("subsirul solutie este:\n\t");
+  for (i = n - 1; i >= 0; i--) {
+    if (D[i] == max && curent_max > v[i]) {
+      printf("%d ", v[i]);
+      max--;
+      curent_max = v[i];
+    }
+  }
+  printf("\n");
+}
+
+int main() {
+  printf("\n---------------------------------------------\n");
+
+  printf("\n--- PROBLEMA RUCSACULUI DISCRET ---\n");
+  int m[] = {10, 30, 20};
+  int v[] = {60, 120, 100};
+  problema_rucsacului_discret(m, v, 3, 50);
+
+  printf("\n--- PROBLEMA CELUI MAI LUNG SUBSIR ---\n");
+  int v1[] = {6, 1, 2, 9, 3, 8, 4, 7};
+  problema_subsir(v1, 8);
+
+  printf("\n---------------------------------------------\n");
+  return 0;
 }
