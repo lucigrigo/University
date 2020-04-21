b72de8c6ec9237769ad3c9b8d082a480650ad5c9
Grigore Lucian <lucigrigo@yahoo.com>
2020-04-21 20:55:42 +0300

    PA lab9

--- PA/Lab9/lab9.c
+++ PA/Lab9/lab9.c
@@ -1,13 +1,17 @@
+/*
+                        Proiectarea Algoritmilor
+                              Laborator 9
+                      Grigore Lucian-Florin 324CDb
+*/
 #include <stdio.h>
 #include <stdlib.h>
-
 #define min(a, b) ((a) < (b) ? (a) : (b))
 
 // graful folosit
-int n = 10;
+int n = 10;    // numarul de noduri din graf
 int G[10][10];
 
-// puncte de articulatie
+// variabilele folosite pentru punctele de articulatie
 int low_art[10];
 int d_art[10];
 int art[10];
@@ -15,9 +19,8 @@ int vizitat_art[10];
 int timp_art;
 int subarb_art[10];
 int p_art[10];
-int pas;
 
-// punti
+// variabilele folosite pentru punti
 int timp_punti;
 int vizitat_punti[10];
 int d_punti[10];
@@ -26,37 +29,44 @@ int low_punti[10];
 int punte[10];
 int nr_punti;
 
+// functia care realizeaza explorarea pentru alg. de determinare a pct.
+// de articulatie
 void exploreaza_art(int u) {
   d_art[u] = low_art[u] = timp_art++;
   vizitat_art[u] = 1;
-  printf("%d: %d d=%d low=%d\n", pas++, u, d_art[u], low_art[u]);
+  // printf("%d: nod %d d=%d low=%d\n", pas++, u, d_art[u], low_art[u]);
   int v;
   for (v = 0; v < n; v++) {
-    if (G[u][v] == 1) {
+    if (G[u][v] == 1 || G[v][u] == 1) {
       if (vizitat_art[v] == 0) {
-        p_art[v] = u;
-        subarb_art[u] += 1;
+        p_art[v] = u; // atribuire parinte nod nou
+        subarb_art[u]++; // actualizare nr subarbori dominati de u
         exploreaza_art(v);
         low_art[u] = min(low_art[u], low_art[v]);
+        // if(low_art[v] < low_art[u]) {
+        //     low_art[u] = low_art[v];
+        // }
         if (p_art[u] != -1 && low_art[v] >= d_art[u]) {
-          art[u] = 1;
+          art[u] = 1; // cazul 2 al teoremei
         }
-      } else if (p_art[u] != v) {
+    } else if (p_art[u] != v) {
         low_art[u] = min(low_art[u], d_art[v]);
+        // low_art[u] = d_art[v];
       }
     }
   }
+    // printf("%d: nod %d d=%d low=%d\n", pas++, u, d_art[u], low_art[u]);
 }
 
-void alg_tarjan() {
+void alg_tarjan() { // functia care determina punctele de articulatie din graf
   int i;
   for (i = 0; i < n; ++i) {
-    p_art[i] = -1;
+    p_art[i] = -1; // valoare default pentru parinte
   }
-  for (i = 0; i < n; ++i) {
-    if (vizitat_art[i] == 0) {
-      exploreaza_art(i);
-      if (subarb_art[i] > 1) {
+  for (i = 0; i < n; ++i) {    // parcurgerea grafului
+    if (vizitat_art[i] == 0) { // daca gasim un nod nevizitat
+      exploreaza_art(i);       // exploram nodul
+      if (subarb_art[i] > 1) { // verificare radacina subarb
         art[i] = 1;
       }
     }
@@ -72,17 +82,18 @@ void alg_tarjan() {
   printf("nr total pct de articulatie = %d\n\n", nr_art);
 }
 
+// functia care exploreaza un nod pt. alg. de determinare a puntilor
 void exploreaza_punti(int u) {
-  d_punti[u] = low_punti[u] = timp_punti++;
-  vizitat_punti[u] = 1;
+  d_punti[u] = low_punti[u] = timp_punti++; // initializari
+  vizitat_punti[u] = 1; // il marcam ca vizitat
   int v;
   for (v = 0; v < n; ++v) {
-    if (G[u][v] == 1) {
+    if (G[u][v] == 1 || G[v][u] == 1) {
       if (vizitat_punti[v] == 0) {
         p_punti[v] = u;
         exploreaza_punti(v);
         low_punti[u] = min(low_punti[u], low_punti[v]);
-        if (low_punti[v] > d_punti[u]) {
+        if (low_punti[v] >= d_punti[u]) {
           punte[v] = 1;
         }
       } else if (p_punti[v] != u) {
@@ -92,24 +103,24 @@ void exploreaza_punti(int u) {
   }
 }
 
-void punti_tarjan() {
+void punti_tarjan() { // functia care determina puntile dintr-un graf
   int i;
   for (i = 0; i < n; ++i) {
-    p_punti[i] = -1;
+    p_punti[i] = -1; // initializare valoare default pentru parinte
   }
-  for (i = 0; i < n; ++i) {
-    if (vizitat_punti[i] == 0) {
-      exploreaza_punti(i);
+  for (i = 0; i < n; ++i) { // parcurgem graful
+    if (vizitat_punti[i] == 0) { // daca am gasit un nod neexplorat
+      exploreaza_punti(i); // il exploram
     }
   }
-  printf("\npuntile sunt:\n");
+  printf("puntile sunt:\n");
   for (i = 0; i < n; ++i) {
     if (punte[i] == 1) {
       printf("\t%d\n", i);
       nr_punti++;
     }
   }
-  printf("nr punti=%d\n\n", nr_punti);
+  printf("nr total punti=%d\n\n", nr_punti);
 }
 
 int main() {
@@ -125,7 +136,10 @@ int main() {
   G[3][5] = 1;
   G[5][6] = 1;
   G[7][8] = 1;
+  printf("\n--- PUNCTE DE ARTICULATIE ---\n");
   alg_tarjan();
+  printf("\n\n--- PUNTI ---\n");
   punti_tarjan();
+  printf("\n");
   return 0;
 }
