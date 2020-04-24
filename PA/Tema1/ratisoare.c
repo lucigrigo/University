5dac61c14f3fe65171124ed2363e0cb1db941503
lucigrigo <lucigrigo@yahoo.com>
2020-04-24 11:43:04 +0300

    Tema1 PA oficial final form

--- PA/Tema1/ratisoare.c
+++ PA/Tema1/ratisoare.c
@@ -1,3 +1,8 @@
+/*
+  Proiectarea Algoritmilor - Tema 1
+  Problema 3 - Ratustele
+  Grigore Lucian-Florin 324CD
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,74 +10,186 @@
 #define INPUT_FILENAME "ratisoare.in"
 #define OUTPUT_FILENAME "ratisoare.out"
 
-struct rata
-{
-    int linie;
-    int coloana;
-    int index_initial;
-    char *orientare;
-    int timp;
+// valoarea default (de initializare) pentru timpul de iesire
+#define DEFAULT_TIME 2147483647
+
+// structura ce memoreaza informatiile despre o rata
+struct rata {
+  int linie;
+  int coloana;
+  int index_initial;
+  int timp_iesire;
+  char *orientare;
 };
 
-struct rata *rata_noua(int l, int c, int i, char *o)
-{
-    struct rata *r = (struct rata *)malloc(sizeof(struct rata));
-    r->orientare = (char *)malloc(2 * sizeof(char));
-    strcpy(r->orientare, o);
-    r->index_initial = i;
-    r->linie = l;
-    r->coloana = c;
-    return r;
+// functia care creeaza o rata noua
+struct rata *rata_noua(int l, int c, int i, char *o) {
+  struct rata *r = (struct rata *)malloc(sizeof(struct rata));
+  r->orientare = (char *)malloc(2 * sizeof(char));
+  strncpy(r->orientare, o, 2);
+  r->index_initial = i;
+  r->linie = l;
+  r->coloana = c;
+  r->timp_iesire = DEFAULT_TIME;
+  return r;
 }
 
-struct rata** calc_timp(){
-    // TODO
-    return NULL;
+// functie de comparare folosita la sortarea initiala
+int init_comp(const void *p, const void *q) {
+  // sortare rate dupa linie si coloana crescator
+  if (((*(struct rata **)p)->linie != (*(struct rata **)q)->linie)) {
+    return ((*(struct rata **)p)->linie - (*(struct rata **)q)->linie);
+  }
+  return ((*(struct rata **)p)->coloana - (*(struct rata **)q)->coloana);
 }
 
-int init_comp(const void *p, const void *q)
-{
-    // TODO
-    // sort rate dupa linie si coloana crescator
-    return 0;
+// functie de comparare folosita la sortarea finala
+int final_comp(const void *p, const void *q) {
+  // sortare rate dupa timpul de iesire
+  if ((*(struct rata **)p)->timp_iesire != (*(struct rata **)q)->timp_iesire) {
+    return (*(struct rata **)p)->timp_iesire -
+           (*(struct rata **)q)->timp_iesire;
+  }
+  return (*(struct rata **)p)->index_initial -
+         (*(struct rata **)q)->index_initial;
 }
 
-int final_comp(const void *p, const void *q){
-    // TODO 
-    // sort rate dupa timpul de iesire
-    return 0;
-}
+// functie care calculeaza timpii de iesire pentru o anumita linie
+struct rata **calcul_linie(struct rata **rate, int start_index,
+                           int linie_curenta, int nr_rate, int nr_coloane) {
+  int i = start_index, index_max = -1;
 
-int main()
-{
-    // valori de input
-    short nr_linii;
-    short nr_coloane;
-    int k;
-    int nr_ratisoare;
+  // scoatem ratele care sunt orientate inspre stanga si pot fi scoase
+  while (i < nr_rate && strcmp(rate[i]->orientare, "S") == 0 &&
+         rate[i]->linie == linie_curenta) {
+    rate[i]->timp_iesire = rate[i]->coloana + 1;
+    i++;
+  }
 
-    // valoare de iesire
-    int index_k;
-    int i;
+  // cautam indicele ultimei rate de pe linia curenta
+  while (i < nr_rate && rate[i]->linie == linie_curenta) {
+    i++;
+  }
+  i--;
+  index_max = i;
 
-    // citire date de intrare
-    FILE *file_in = fopen(INPUT_FILENAME, "r");
-    fscanf(file_in, "%hi %hi %d %d\n", &nr_linii, &nr_coloane, &k, &nr_ratisoare);
-    struct rata **rate = (struct rata **)malloc(nr_ratisoare * sizeof(struct rata *));
-    for (i = 0; i < nr_ratisoare; ++i)
-    {
-        int identificator;
-        int index_linie;
-        int index_coloana;
-        char *orientare;
-        fscanf(file_in, "%d %d %d %c\n", &identificator, &index_linie, &index_coloana, orientare);
-        rate[i] = rata_noua(index_linie, index_coloana, i, orientare);
+  // scoatem ratele care sunt orientate inspre dreapta si pot fi scoase
+  while (i >= start_index && strcmp(rate[i]->orientare, "D") == 0 &&
+         rate[i]->linie == linie_curenta) {
+    rate[i]->timp_iesire = nr_coloane - rate[i]->coloana;
+    i--;
+  }
+  // numarul maxim posibil de rate de pe linie
+  int max_dim = index_max - start_index + 1;
+
+  // alocare memorie pentru vectorii de pointeri
+  struct rata **orientate_stanga =
+      (struct rata **)malloc(max_dim * sizeof(struct rata *));
+  struct rata **orientate_dreapta =
+      (struct rata **)malloc(max_dim * sizeof(struct rata *));
+  int max_stanga = 0, max_dreapta = 0;
+
+  // construirea vectorilor de pointeri cu cele doua tipuri de orientari
+  for (i = start_index; i <= index_max; ++i) {
+    if (rate[i]->timp_iesire == DEFAULT_TIME) {
+      // daca rata nu a fost scoasa la pasul precedent
+      if (strcmp(rate[i]->orientare, "D") == 0) {
+        // daca rata este ->
+        orientate_dreapta[max_dreapta] = rate[i];
+        max_dreapta++;
+      } else {
+        // daca rata este <-
+        orientate_stanga[max_stanga] = rate[i];
+        max_stanga++;
+      }
     }
-    fclose(file_in);
+  }
 
-    // scriere date de iesire
-    FILE *file_out = fopen(OUTPUT_FILENAME, "w");
-    fprintf(file_out, "%d", index_k);
-    fclose(file_out);
-    return 0;
+  // atribuirea primelor max_stanga rate care nu au timpul de iesire calculat
+  // timpul de iesire al primelor max_stanga rate orientate la stanga
+  int st = 0, dr = 0;
+  for (i = start_index; i <= index_max && st < max_stanga; ++i) {
+    struct rata *r = rate[i];
+    if (r->timp_iesire == DEFAULT_TIME) {
+      r->timp_iesire = orientate_stanga[st]->coloana + 1;
+      st++;
+    }
+  }
+
+  // atribuirea ratelor ramase cu timpul necalculat timpul de iesire al ratelor
+  // orientate la dreapta
+  for (; i <= index_max && dr < max_dreapta; ++i) {
+    struct rata *r = rate[i];
+    if (r->timp_iesire == DEFAULT_TIME) {
+      r->timp_iesire = nr_coloane - orientate_dreapta[dr]->coloana;
+      dr++;
+    }
+  }
+
+  // eliberarea vectorilor
+  free(orientate_dreapta);
+  free(orientate_stanga);
+  return rate;
+}
+
+int main() {
+  int i;
+
+  // valori de input
+  int nr_linii;
+  int nr_coloane;
+  int k;
+  int nr_ratisoare;
+
+  // citire date de intrare
+  FILE *file_in = fopen(INPUT_FILENAME, "r");
+  fscanf(file_in, "%d %d %d %d\n", &nr_linii, &nr_coloane, &k, &nr_ratisoare);
+  struct rata **rate =
+      (struct rata **)malloc(nr_ratisoare * sizeof(struct rata *));
+  char *orientare = (char *)malloc(2 * sizeof(char));
+  for (i = 0; i < nr_ratisoare; ++i) {
+    int identificator;
+    int index_linie;
+    int index_coloana;
+    fscanf(file_in, "%d %d %d %c\n", &identificator, &index_linie,
+           &index_coloana, orientare);
+    rate[i] =
+        rata_noua(index_linie - 1, index_coloana - 1, identificator, orientare);
+  }
+  fclose(file_in);
+
+  // sortare rate dupa linii si coloane
+  qsort(rate, nr_ratisoare, sizeof(struct rata *), init_comp);
+
+  // parcurgerea pe linii si calculul pe fiecare linie la fiecare rata
+  int index;
+  for (i = 0, index = 0; i < nr_linii && index < nr_ratisoare; i++) {
+    // daca pe linia curenta nu avem nicio rata
+    while (rate[index]->linie != i) {
+      i++;
+    }
+    rate = calcul_linie(rate, index, i, nr_ratisoare, nr_coloane);
+
+    // gasim prima rata care nu este de pe linia curenta
+    while (index < nr_ratisoare && rate[index]->linie == i) {
+      index++;
+    }
+  }
+
+  // sortare rate crescator dupa timpul de iesire si index
+  qsort(rate, nr_ratisoare, sizeof(struct rata *), final_comp);
+
+  // scriere date de iesire
+  FILE *file_out = fopen(OUTPUT_FILENAME, "w");
+  fprintf(file_out, "%d", rate[k - 1]->index_initial);
+  fclose(file_out);
+
+  // eliberarea memoriei
+  for (i = 0; i < nr_ratisoare; ++i) {
+    free(rate[i]->orientare);
+    free(rate[i]);
+  }
+  free(rate);
+
+  return 0;
 }
--- PA/Tema1/teme.c
+++ PA/Tema1/teme.c
@@ -1,3 +1,8 @@
+/*
+  Proiectarea Algoritmilor - Tema 1
+  Problema 1 - Temele buclucase
+  Grigore Lucian-Florin 324CD
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -5,163 +10,207 @@
 #define INPUT_FILENAME "teme.in"
 #define OUTPUT_FILENAME "teme.out"
 
-struct tema
-{
-    short punctaj;
-    short timp_rezolvare;
-    short deadline;
-    short index;
-    int rezolvata;
+// structura care memoreaza informatiile despre o tema
+struct tema {
+  short punctaj;
+  short timp_rezolvare;
+  short deadline;
+  short index;
 };
 
-struct tema *tema_noua(short p, short tr, short d, short i)
-{
-    struct tema *t = (struct tema *)malloc(sizeof(struct tema));
-    t->deadline = d;
-    t->punctaj = p;
-    t->index = i;
-    t->timp_rezolvare = tr;
-    t->rezolvata = 0;
-    return t;
+// structura care memoreaza informatiile legate de o solutie
+struct solutie {
+  int punctaj;
+  int no_teme;
+  int index_i;
+  int index_j;
+  int rezolvata;
+  struct solutie *prev;
+};
+
+// functie care creeaza o noua tema
+struct tema *tema_noua(short p, short tr, short d, short i) {
+  struct tema *t = (struct tema *)malloc(sizeof(struct tema));
+  t->deadline = d;
+  t->punctaj = p;
+  t->index = i;
+  t->timp_rezolvare = tr;
+  return t;
 }
 
-int tcomp(const void *p, const void *q)
-{
-    struct tema *t1 = *(struct tema **)p;
-    struct tema *t2 = *(struct tema **)q;
-    if (t1->deadline != t2->deadline)
-    {
-        return t1->deadline - t2->deadline;
-    }
-    if (t1->punctaj != t2->punctaj)
-    {
-        return t2->punctaj - t1->punctaj;
-    }
-    return t1->index - t2->index;
+// functie care creeaza un nou element care memoreaza o solutie
+struct solutie *new_sol(int add, const int max_size) {
+  struct solutie *s = (struct solutie *)malloc(sizeof(struct solutie));
+  s->punctaj = 0;
+  s->no_teme = 0;
+  s->index_i = -1;
+  s->index_j = -1;
+  s->rezolvata = 0;
+  s->prev = NULL;
+  return s;
 }
 
-int main()
-{
-    // valoare de input
-    short nr_teme_total;
-    short final_deadline;
+// functie de comparare intre teme folosita pentru qsort
+int tcomp(const void *p, const void *q) {
+  // temele sunt comparate dupa deadline
+  return (*(struct tema **)p)->deadline - (*(struct tema **)q)->deadline;
+}
 
-    // valori de output
-    int i;
+struct solutie *alegere(struct solutie *s1, struct solutie *s2,
+                        struct tema *tema_curenta, int ii, int j) {
+  // initializare solutie
+  struct solutie *sol = new_sol(0, 0);
+  // linia curenta
+  sol->index_i = ii;
+  // coloana curenta
+  sol->index_j = j;
 
-    // citire date de intrare
-    FILE *file_in = fopen(INPUT_FILENAME, "r");
-    fscanf(file_in, "%hi\n", &nr_teme_total);
-    struct tema **teme = (struct tema **)malloc(nr_teme_total * sizeof(struct tema *));
-    for (i = 0; i < nr_teme_total; ++i)
-    {
-        short timp_rezolvare;
-        short punctaj;
-        short deadline;
-        fscanf(file_in, "%hi %hi %hi\n", &timp_rezolvare, &punctaj, &deadline);
-        teme[i] = tema_noua(punctaj, timp_rezolvare, deadline, i + 1);
-        final_deadline = max(final_deadline, deadline);
+  // daca nu exista o tema ce poate fi rezolvata, alegem maximul dintre solutii
+  if (tema_curenta == NULL) {
+    if (s1->punctaj > s2->punctaj) {
+      sol->punctaj = s1->punctaj;
+      sol->no_teme = s1->no_teme;
+      sol->prev = s1;
+    } else {
+      sol->punctaj = s2->punctaj;
+      sol->no_teme = s2->no_teme;
+      sol->prev = s2;
     }
-    fclose(file_in);
-    // final_deadline--;
-
-    // sortarea temelor
-    qsort(teme, nr_teme_total, sizeof(struct tema *), tcomp);
-
-    for (i = 0; i < nr_teme_total; ++i)
-    {
-        printf("%d - #%d t:%d d:%d p:%d\n", i, teme[i]->index, teme[i]->timp_rezolvare, teme[i]->deadline, teme[i]->punctaj);
+  } else {
+    // daca putem face o tema, alegem maximul dintre D[i-1][j] si D[i-1][j-t_f]
+    if (s2->punctaj + tema_curenta->punctaj > s1->punctaj) {
+      // daca este mai profitabil sa facem tema curenta in loc de oricare alta
+      sol->punctaj = s2->punctaj + tema_curenta->punctaj;
+      sol->no_teme = s2->no_teme + 1;
+      sol->prev = s2;
+      sol->rezolvata = 1;
+    } else {
+      // este mai profitabil sa nu facem tema curenta
+      sol->punctaj = s1->punctaj;
+      sol->no_teme = s1->no_teme;
+      sol->prev = s1;
     }
-    printf("\n\n");
+  }
 
-    // alegerea temelor ce trebuie rezolvate
-    int sapt_curenta = 0;
-    int teme_rezolvate = 0;
-    int punctaj_acumulat = 0;
-    i = 0;
-    while (sapt_curenta < final_deadline)
-    {
-        if (i >= nr_teme_total || teme[i] == NULL)
-            break;
-        struct tema *tema_curenta = teme[i];
-        // printf("- curent #%d, ddl=%d\n", tema_curenta->index, sapt_curenta);
-        if (tema_curenta->deadline <= sapt_curenta)
-        {
-            i++;
-            continue;
-        }
-        if (sapt_curenta + tema_curenta->timp_rezolvare <= tema_curenta->deadline && tema_curenta->rezolvata != 1)
-        {
-            int j = i + 1;
-            printf("%d\n", i);
-            int max_gasit = tema_curenta->punctaj;
-            int index_max = -1;
-            while (j < nr_teme_total && tema_curenta->deadline + teme[j]->timp_rezolvare >= teme[j]->deadline)
-            {
-                if (teme[j]->punctaj > max_gasit)
-                {
-                    index_max = j;
-                    max_gasit = teme[j]->punctaj;
-                }
-                j++;
-            }
-            printf("\n\tmax=%d index=%d\n", max_gasit, index_max);
-            if (index_max != -1)
-            {
-                if (tema_curenta->timp_rezolvare + teme[index_max]->timp_rezolvare + sapt_curenta <= teme[index_max]->deadline && sapt_curenta + tema_curenta->timp_rezolvare <= tema_curenta->deadline && teme[index_max]->deadline - tema_curenta->deadline <= teme[index_max]->timp_rezolvare)
-                {
-                    sapt_curenta += tema_curenta->timp_rezolvare + teme[index_max]->timp_rezolvare;
-                    punctaj_acumulat += tema_curenta->punctaj;
-                    punctaj_acumulat += teme[index_max]->punctaj;
-                    tema_curenta->rezolvata = 1;
-                    teme[index_max]->rezolvata = 1;
-                    i = index_max;
-                    printf("\tamandoua\n");
-                }
-                else if (tema_curenta->punctaj > teme[j]->punctaj)
-                {
-                    tema_curenta->rezolvata = 1;
-                    punctaj_acumulat += tema_curenta->punctaj;
-                    sapt_curenta += tema_curenta->timp_rezolvare;
-                    printf("\t%d\n", i);
-                }
-                else
-                {
-                    teme[index_max]->rezolvata = 1;
-                    punctaj_acumulat += teme[index_max]->punctaj;
-                    sapt_curenta += teme[index_max]->timp_rezolvare;
-                    i = index_max;
-                    printf("\t%d\n", index_max);
-                }
-            }
-            else
-            {
-                tema_curenta->rezolvata = 1;
-                punctaj_acumulat += tema_curenta->punctaj;
-                sapt_curenta += tema_curenta->timp_rezolvare;
-            }
-            printf("%d\n", i);
-            i++;
-        }
-        else
-        {
-            ++i;
-        }
-    }
+  // intoarcem solutie obtinuta
+  return sol;
+}
 
-    // scriere date de iesire
-    FILE *file_out = fopen(OUTPUT_FILENAME, "w");
-    printf("\n -- REZULTATE FINALE\n");
-    fprintf(file_out, "%d %d\n", punctaj_acumulat, teme_rezolvate);
-    printf("%d %d\n", punctaj_acumulat, teme_rezolvate);
-    for (i = 0; i < nr_teme_total; ++i)
-    {
-        if (teme[i]->rezolvata == 1)
-        {
-            fprintf(file_out, "%hi ", teme[i]->index);
-            printf("%hi ", teme[i]->index);
+int main() {
+  int i;
+
+  // valoare de input
+  short nr_teme_total;
+  short final_deadline = 0;
+
+  // citire date de intrare
+  FILE *file_in = fopen(INPUT_FILENAME, "r");
+  fscanf(file_in, "%hi\n", &nr_teme_total);
+  struct tema **teme =
+      (struct tema **)malloc(nr_teme_total * sizeof(struct tema *));
+  for (i = 0; i < nr_teme_total; ++i) {
+    short timp_rezolvare;
+    short punctaj;
+    short deadline;
+    fscanf(file_in, "%hi %hi %hi\n", &timp_rezolvare, &punctaj, &deadline);
+    teme[i] = tema_noua(punctaj, timp_rezolvare, deadline, i + 1);
+    final_deadline = max(final_deadline, deadline);
+  }
+  fclose(file_in);
+
+  // sortarea temelor crescator dupa deadline
+  qsort(teme, nr_teme_total, sizeof(struct tema *), tcomp);
+
+  // alocare memorie pentru matricea de solutii
+  struct solutie ***D = (struct solutie ***)malloc((nr_teme_total + 2) *
+                                                   sizeof(struct solutie **));
+  for (i = 0; i <= nr_teme_total; ++i) {
+    D[i] = (struct solutie **)malloc((final_deadline + 2) *
+                                     sizeof(struct solutie *));
+  }
+
+  // initializare valori default pentru cazul de baza
+  for (i = 0; i <= final_deadline; ++i) {
+    D[0][i] = new_sol(1, nr_teme_total);
+  }
+
+  // construirea solutiei optime la fiecare pas
+  for (i = 1; i <= (nr_teme_total); ++i) {
+    int j;
+    // indexul temei curente
+    int index_tema = i - 1;
+    // tema curenta
+    struct tema *tema_curenta = teme[index_tema];
+    // deadline temei curente
+    int ddl = tema_curenta->deadline;
+    // timpul de finalizare al temei curente
+    int t_f = tema_curenta->timp_rezolvare;
+
+    // parcurgere pe parcursul saptamanilor
+    for (j = 0; j <= final_deadline; j++) {
+      if (j < t_f) {
+        // daca nu putem face tema curenta (dpdv al timpului)
+        // copiem solutia de mai sus (aceeasi saptamana, tema precedenta)
+        D[i][j] = new_sol(0, 0);
+        D[i][j]->no_teme = D[i - 1][j]->no_teme;
+        D[i][j]->prev = D[i - 1][j];
+        D[i][j]->punctaj = D[i - 1][j]->punctaj;
+        D[i][j]->index_i = i;
+        D[i][j]->index_j = j;
+      } else {
+        if (j <= ddl) {
+          // daca inca putem face tema curenta
+          // alegem maximul dintre D[i-1][j] si D[i-1][j-t_r]
+          D[i][j] = alegere(D[i - 1][j], D[i - 1][j - t_f], tema_curenta, i, j);
+        } else {
+          // daca am trecut de deadline-ul temei curente
+          // alegem maximul dintre D[i-1][j] si D[i][j-1]
+          D[i][j] = alegere(D[i - 1][j], D[i][j - 1], NULL, i, j);
         }
+      }
     }
-    fclose(file_out);
-    return 0;
+  }
+
+  // rezultatele finale
+  int punctaj_acumulat = D[nr_teme_total][final_deadline]->punctaj;
+  int teme_rezolvate = D[nr_teme_total][final_deadline]->no_teme;
+
+  // scriere date de iesire
+  FILE *file_out = fopen(OUTPUT_FILENAME, "w");
+  fprintf(file_out, "%d %d\n", punctaj_acumulat, teme_rezolvate);
+
+  // construirea solutiei in ordinea rezolvarii temelor
+  struct solutie *sol_curenta = D[nr_teme_total][final_deadline];
+  int *sol = (int *)malloc(nr_teme_total * sizeof(int));
+  i = 0;
+  while (sol_curenta != NULL) {
+    if (sol_curenta->rezolvata == 1) {
+      sol[i++] = teme[sol_curenta->index_i - 1]->index;
+    }
+    sol_curenta = sol_curenta->prev;
+  }
+  i--;
+
+  // printarea solutiei
+  while (i >= 0) {
+    fprintf(file_out, "%d ", sol[i--]);
+  }
+  fclose(file_out);
+
+  // eliberarea memoriei folosite
+  free(sol);
+  for (i = 0; i <= nr_teme_total; ++i) {
+    int j = 0;
+    if (D[i] != NULL) {
+      for (j = 0; j <= final_deadline; ++j) {
+        if (D[i][j] != NULL) {
+          free(D[i][j]);
+        }
+      }
+      free(D[i]);
+    }
+  }
+  free(D);
+
+  return 0;
 }
