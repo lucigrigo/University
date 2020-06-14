3b92766f99b5dea10a8725f1f1851b3339310647
lucigrigo <lucigrigo@yahoo.com>
2020-06-14 19:52:09 +0300

    Update cses.cpp

--- TestSources/C++/cses.cpp
+++ TestSources/C++/cses.cpp
@@ -3,21 +3,13 @@
 using namespace std;
 
 int main() {
-    int n;
+    long n;
     cin >> n;
+    long ans = 0;
 
-    if(n == 1)
-        cout << 1;
-    else if(n <= 3)
-        cout << "NO SOLUTION";
-    else {
-        int m = n / 2 + 1;
-        int a = 1;
+    //ans += n / 10;
+    ans += n / 5;
 
-        while(a <= n / 2 || m <= n) {
-            cout << a << " " << m << " ";
-            ++a; ++m;
-        }
-    }
+    cout << ans;
     return 0;
 }
