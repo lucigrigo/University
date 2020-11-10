a12e6d71106cefd4be47d4828f6beb23b35f4fef
Grigore Lucian <luci.grigore99@gmail.com>
2020-11-10 18:23:17 +0200

    start tema1 EGC

---
+++ 3rd Year/EGC - Computer Graphics/Framework-EGC-master/Source/Laboratoare/Tema1/Tema1.cpp
@@ -0,0 +1,67 @@
+#include "Tema1.h"
+
+Tema1::Tema1()
+{
+}
+
+Tema1::~Tema1()
+{
+}
+
+void Tema1::Init()
+{
+	
+}
+
+void Tema1::FrameStart()
+{
+	
+}
+
+void Tema1::Update(float deltaTimeSeconds)
+{
+	
+}
+
+void Tema1::FrameEnd()
+{
+
+}
+
+void Tema1::OnInputUpdate(float deltaTime, int mods)
+{
+	
+}
+
+void Tema1::OnKeyPress(int key, int mods)
+{
+	
+}
+
+void Tema1::OnKeyRelease(int key, int mods)
+{
+	// add key release event
+}
+
+void Tema1::OnMouseMove(int mouseX, int mouseY, int deltaX, int deltaY)
+{
+	// add mouse move event
+}
+
+void Tema1::OnMouseBtnPress(int mouseX, int mouseY, int button, int mods)
+{
+	// add mouse button press event
+}
+
+void Tema1::OnMouseBtnRelease(int mouseX, int mouseY, int button, int mods)
+{
+	// add mouse button release event
+}
+
+void Tema1::OnMouseScroll(int mouseX, int mouseY, int offsetX, int offsetY)
+{
+}
+
+void Tema1::OnWindowResize(int width, int height)
+{
+}
