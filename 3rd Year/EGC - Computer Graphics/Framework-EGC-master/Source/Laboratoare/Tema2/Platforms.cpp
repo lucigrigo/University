d0b73a35e9227428aee91543bd2b8f28490aeeb7
Grigore Lucian <lucigrigo@yahoo.com>
2020-12-10 14:19:36 +0200

    Create Platforms.cpp

---
+++ 3rd Year/EGC - Computer Graphics/Framework-EGC-master/Source/Laboratoare/Tema2/Platforms.cpp
@@ -0,0 +1,18 @@
+#include "Platforms.h"
+
+using namespace std;
+
+int SpawnNextPlatforms(int far_z,
+                       std::vector<glm::vec3> positions)
+{
+    return 1;
+}
+
+int SpawnNextPlatforms(int far_z,
+                       std::vector<glm::vec3> positions,
+                       std::vector<glm::vec3> colors,
+                       std::vector<PLATFORM_TYPES> types,
+                       std::vector<bool> it_affects)
+{
+    return 1;
+}
