8c5da9412cfec349e34dffc95fbbf49afa5fa16e
Grigore Lucian <luci.grigore99@gmail.com>
2021-03-04 09:41:02 +0200

    start lab asc

---
+++ 3rd Year/ASC - Computing Systems Architecture/Labs/Lab01/coffee_maker.py
@@ -0,0 +1,85 @@
+"""
+A command-line controlled coffee maker.
+"""
+
+import sys
+
+"""
+Implement the coffee maker's commands. Interact with the user via stdin and print to stdout.
+
+Requirements:
+    - use functions
+    - use __main__ code block
+    - access and modify dicts and/or lists
+    - use at least once some string formatting (e.g. functions such as strip(), lower(),
+    format()) and types of printing (e.g. "%s %s" % tuple(["a", "b"]) prints "a b"
+    - BONUS: read the coffee recipes from a file, put the file-handling code in another module
+    and import it (see the recipes/ folder)
+
+There's a section in the lab with syntax and examples for each requirement.
+
+Feel free to define more commands, other coffee types, more resources if you'd like and have time.
+"""
+
+"""
+Tips:
+*  Start by showing a message to the user to enter a command, remove our initial messages
+*  Keep types of available coffees in a data structure such as a list or dict
+e.g. a dict with coffee name as a key and another dict with resource mappings (resource:percent)
+as value
+"""
+
+# Commands
+EXIT = "exit"
+LIST_COFFEES = "list"
+MAKE_COFFEE = "make"  #!!! when making coffee you must first check that you have enough resources!
+HELP = "help"
+REFILL = "refill"
+RESOURCE_STATUS = "status"
+commands = [EXIT, LIST_COFFEES, MAKE_COFFEE, REFILL, RESOURCE_STATUS, HELP]
+
+# Coffee examples
+ESPRESSO = "espresso"
+AMERICANO = "americano"
+CAPPUCCINO = "cappuccino"
+
+# Resources examples
+WATER = "water"
+COFFEE = "coffee"
+MILK = "milk"
+
+# Coffee maker's resources - the values represent the fill percents
+RESOURCES = {WATER: 100, COFFEE: 100, MILK: 100}
+
+"""
+Example result/interactions:
+
+I'm a smart coffee maker
+Enter command:
+list
+americano, cappuccino, espresso
+Enter command:
+status
+water: 100%
+coffee: 100%
+milk: 100%
+Enter command:
+make
+Which coffee?
+espresso
+Here's your espresso!
+Enter command:
+refill
+Which resource? Type 'all' for refilling everything
+water
+water: 100%
+coffee: 90%
+milk: 100%
+Enter command:
+exit
+"""
+
+print("I'm a simple coffee maker")
+print("Press enter")
+sys.stdin.readline()
+print("Teach me how to make coffee...please...")
---
+++ 3rd Year/ASC - Computing Systems Architecture/Labs/Lab01/load_recipes.py
@@ -0,0 +1,13 @@
+"""
+	Bonus task: load all the available coffee recipes from the folder 'recipes/'
+	File format:
+		first line: coffee name
+		next lines: resource=percentage
+
+	info and examples for handling files:
+		http://cs.curs.pub.ro/wiki/asc/asc:lab1:index#operatii_cu_fisiere
+		https://docs.python.org/3/library/io.html
+		https://docs.python.org/3/library/os.path.html
+"""
+
+RECIPES_FOLDER = "recipes"
