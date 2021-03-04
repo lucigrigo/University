c644c377315c8cd197241fc2a3e6b7aabc9f9540
Grigore Lucian <lucigrigo@yahoo.com>
2021-03-04 13:30:08 +0200

    asc lab01

--- 3rd Year/ASC - Computing Systems Architecture/Labs/Lab01/coffee_maker.py
+++ 3rd Year/ASC - Computing Systems Architecture/Labs/Lab01/coffee_maker.py
@@ -3,6 +3,7 @@ A command-line controlled coffee maker.
 """
 
 import sys
+from load_recipes import import_recipes
 
 """
 Implement the coffee maker's commands. Interact with the user via stdin and print to stdout.
@@ -19,9 +20,7 @@ Requirements:
 There's a section in the lab with syntax and examples for each requirement.
 
 Feel free to define more commands, other coffee types, more resources if you'd like and have time.
-"""
 
-"""
 Tips:
 *  Start by showing a message to the user to enter a command, remove our initial messages
 *  Keep types of available coffees in a data structure such as a list or dict
@@ -32,7 +31,8 @@ as value
 # Commands
 EXIT = "exit"
 LIST_COFFEES = "list"
-MAKE_COFFEE = "make"  #!!! when making coffee you must first check that you have enough resources!
+# !!! when making coffee you must first check that you have enough resources!
+MAKE_COFFEE = "make"
 HELP = "help"
 REFILL = "refill"
 RESOURCE_STATUS = "status"
@@ -50,6 +50,9 @@ MILK = "milk"
 
 # Coffee maker's resources - the values represent the fill percents
 RESOURCES = {WATER: 100, COFFEE: 100, MILK: 100}
+
+# reading recipes from file
+recipes = import_recipes()
 
 """
 Example result/interactions:
@@ -79,7 +82,100 @@ Enter command:
 exit
 """
 
-print("I'm a simple coffee maker")
-print("Press enter")
-sys.stdin.readline()
-print("Teach me how to make coffee...please...")
+
+def f_list_coffees():
+    print('Available coffee types:')
+
+    for coffee in recipes.keys():
+        print(f"\t- {coffee}")
+
+
+def f_make_coffee():
+    coffee = input('Which coffee? ')
+
+    if coffee not in recipes.keys():
+        print('Invalid coffee type')
+        return
+
+    recipe = recipes.get(coffee)
+    ok = True
+
+    for resource, quantity in recipe.items():
+        if RESOURCES[resource] < int(quantity):
+            print(
+                'Not enough {}. Consider refilling me sometimes :('.format(resource))
+            ok = False
+
+    if not ok:
+        return
+
+    for resource, quantity in recipe.items():
+        RESOURCES[resource] = RESOURCES[resource] - int(quantity)
+
+    print('Here\'s your %s!' % coffee)
+
+
+def f_help():
+    print('Commands available:')
+    print('\t- \'exit\' - close the program')
+    print('\t- \'list\' - list available coffees')
+    print('\t- \'make\' - brew coffee')
+    print('\t- \'help\' - list all available commands')
+    print('\t- \'refill\' - refill one or more resources')
+    print('\t- \'status\' - show current status of all the resources')
+
+
+def f_refill():
+    resource = input('Which resource? Type \'all\' for refilling everything: ')
+
+    if resource == 'all':
+        RESOURCES[WATER] = 100
+        RESOURCES[MILK] = 100
+        RESOURCES[COFFEE] = 100
+    elif resource == COFFEE:
+        RESOURCES[COFFEE] = 100
+    elif resource == MILK:
+        RESOURCES[MILK] = 100
+    elif resource == WATER:
+        RESOURCES[WATER] = 100
+
+    if resource != 'all' and resource != WATER and resource != MILK and resource != COFFEE:
+        print('Invalid resource! Try again...')
+    else:
+        print('Refill successful! New status of the resources:')
+        f_resource_status()
+
+
+def f_resource_status():
+    print('Available resources:')
+
+    for resource, percentage in RESOURCES.items():
+        print(f'\t- {resource}: {percentage}%')
+
+
+def f_invalid_cmd():
+    print('You entered an invalid command! Please use a valid command:')
+    f_help()
+
+
+f_commands = {
+    LIST_COFFEES: f_list_coffees,
+    MAKE_COFFEE: f_make_coffee,
+    HELP: f_help,
+    REFILL: f_refill,
+    RESOURCE_STATUS: f_resource_status
+}
+
+if __name__ == '__main__':
+    print("I'm a smart coffee maker!")
+
+    while True:
+        cmd = input("Enter command: ")
+
+        if cmd == EXIT:
+            print('Exiting program')
+            break
+
+        f_cmd = f_commands.get(cmd, f_invalid_cmd)
+
+        f_cmd()
--- 3rd Year/ASC - Computing Systems Architecture/Labs/Lab01/load_recipes.py
+++ 3rd Year/ASC - Computing Systems Architecture/Labs/Lab01/load_recipes.py
@@ -10,4 +10,29 @@
 		https://docs.python.org/3/library/os.path.html
 """
 
+import os
+import os.path
+
 RECIPES_FOLDER = "recipes"
+
+
+def import_recipes():
+    recipes = {}
+
+    if not os.path.isdir(RECIPES_FOLDER):
+        print('Given path is not a folder!')
+        return
+
+    for input_file in os.listdir(RECIPES_FOLDER):
+        with open(RECIPES_FOLDER + '/' + input_file, 'r') as recipe:
+            curr_recipe = {}
+            name = recipe.readline()[:-1]
+
+            for line in recipe:
+                l = line.split('=')
+                l[1] = l[1][:-1]
+                curr_recipe[l[0]] = l[1]
+
+            recipes[name] = curr_recipe
+
+    return recipes
