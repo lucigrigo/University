f8afdd410139fc196a03e471fa7667d7039beee6
Grigore Lucian <lucigrigo@yahoo.com>
2021-05-18 15:53:10 +0300

    lab10 so

---
+++ 3rd Year/SO - Operating Systems/Labs/Lab10/4/iocp.c
@@ -0,0 +1,66 @@
+/*
+ * SO, 2016 - Lab 10 (Advanced I/O Windows
+ * I/O completion ports wrapper functions
+ * Task #4 (I/O completion ports)
+ */
+
+#include <windows.h>
+
+#include "iocp.h"
+
+/*
+ * create I/O completion port
+ */
+
+HANDLE iocp_init(void)
+{
+	/* TODO */
+	return CreateIoCompletionPort(
+			INVALID_HANDLE_VALUE,
+			NULL,
+			(ULONG_PTR) NULL,
+			0);
+}
+
+/*
+ * add handle to completion port; use handle as key
+ */
+
+HANDLE iocp_add(HANDLE iocp, HANDLE hFile)
+{
+	/* TODO */
+	return CreateIoCompletionPort(
+			hFile,
+			iocp,
+			(ULONG_PTR) hFile,
+			0);
+}
+
+/*
+ * add handle to completion port; use key as key
+ */
+
+HANDLE iocp_add_key(HANDLE iocp, HANDLE hFile, ULONG_PTR key)
+{
+	/* TODO */
+	return CreateIoCompletionPort(
+			hFile,
+			iocp,
+			key,
+			0);
+}
+
+/*
+ * wait for event notification on completion port
+ */
+
+BOOL iocp_wait(HANDLE iocp, PDWORD bytes, PULONG_PTR key, LPOVERLAPPED *op)
+{
+	/* TODO */
+	return GetQueuedCompletionStatus(
+			iocp,
+			bytes,
+			key,
+			op,
+			INFINITE);
+}
